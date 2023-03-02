c313aab53cb35a8406d1d4bdc500474d353f15fc
StevenZhangzhexu <e0134151@u.nus.edu>
2023-03-02 13:19:49 +0800

    Change existing file

--- job_scraper.py
+++ job_scraper.py
@@ -9,6 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options() 
 options.add_argument("--headless=new")
+options.add_argument('--no-sandbox')
 options.add_experimental_option("useAutomationExtension", False) 
 service=Service(ChromeDriverManager().install())
 
